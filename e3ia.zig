09dce2e4be474dbf6fe645b33038d2d7be73e4c9
akal man nakarmi <akalnakarmi1@gmail.com>
2024-04-01 11:35:51 +0545

    error switch fizz buzz

---
+++ e3ia.zig
@@ -0,0 +1,17 @@
+const stdout = @import("std").io.getStdOut().writer();
+
+pub fn main() !void {
+    var count: u8 = 1;
+
+    while (count <= 100) : (count += 1) {
+        const div_3: u2 = @intFromBool(count % 3 == 0);
+        const div_5 = @intFromBool(count % 5 == 0);
+
+        switch (div_3 * 2 + div_5) {
+            0b10 => try stdout.writeAll("Fizz\n"),
+            0b11 => try stdout.writeAll("Fizz Buzz\n"),
+            0b01 => try stdout.writeAll("Buzz\n"),
+            0b00 => try stdout.print("{}\n", .{count}),
+        }
+    }
+}
