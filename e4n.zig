7225ba210588a5934d279a31045815a7a40e7397
akal man nakarmi <akalnakarmi1@gmail.com>
2024-04-03 13:45:25 +0545

    more formatting

---
+++ e4n.zig
@@ -0,0 +1,42 @@
+const std = @include("std");
+const bufPrint = std.fmt.bufPrint;
+
+test "position" {
+    var b: [3]u8 = undefined;
+    try expect(eql(
+        u8,
+        try bufPrint(&b, "{0s}{0s}{1s}", .{ "a", "b" }),
+        "aab",
+    ));
+}
+
+test "fill, alignment, width" {
+    var b: [6]u8 = undefined;
+
+    try expect(eql(
+        u8,
+        try bufPrint(&b, "{s: <5}", .{"hi!"}),
+        "hi!  ",
+    ));
+
+    try expect(eql(
+        u8,
+        try bufPrint(&b, "{s:_^6}", .{"hi!"}),
+        "_hi!__",
+    ));
+
+    try expect(eql(
+        u8,
+        try bufPrint(&b, "{s:!>4}", .{"hi!"}),
+        "!hi!",
+    ));
+}
+
+test "precision" {
+    var b: [4]u8 = undefined;
+    try expect(eql(
+        u8,
+        try bufPrint(&b, "{d:.2}", .{3.14159}),
+        "3.14",
+    ));
+}
